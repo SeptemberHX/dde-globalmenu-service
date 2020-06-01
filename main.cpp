ec66a8877ba64a568f7e4d51fa50855428906c2d
SeptemberHX <september_hx@outlook.com>
2020-06-01 22:19:47 +0800

    com.canonical.AppMenu.Registrar

---
+++ main.cpp
@@ -0,0 +1,35 @@
+#include <QApplication>
+#include <QDBusConnection>
+#include <QDBusConnectionInterface>
+#include "menuimporter.h"
+#include "menuimporteradaptor.h"
+
+static const char* DBUS_SERVICE = "com.canonical.AppMenu.Registrar";
+static const char* DBUS_OBJECT_PATH = "/com/canonical/AppMenu/Registrar";
+
+int main(int argc, char *argv[]) {
+    QApplication app(argc, argv);
+
+    /*
+      Create a Chat instance and register it with the session bus only if
+      the service isn't already available.
+    */
+    auto *m_menuImporter = new MenuImporter(&app);
+    m_menuImporter->connectToBus();
+//    QDBusConnection connection = QDBusConnection::sessionBus();
+//    if (!connection.interface()->isServiceRegistered(DBUS_SERVICE))
+//    {
+//        auto *m_menuAdaptor = new MenuImporterAdaptor(m_menuImporter);
+//        if (!connection.registerService(DBUS_SERVICE))
+//        {
+//            qFatal("Could not register service!");
+//        }
+//
+//        if (!connection.registerObject(DBUS_OBJECT_PATH, m_menuImporter))
+//        {
+//            qFatal("Could not register Chat object!");
+//        }
+//    }
+
+    return app.exec();
+}
